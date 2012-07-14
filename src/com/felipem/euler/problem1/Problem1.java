28f54e05db1f204a03aa44692017ffe1d3d3f6e7
Felipe Maza <felipe@felipem.com>
2012-07-14 22:56:42 +0200

    Problema 1 resuelto

---
+++ src/com/felipem/euler/problem1/Problem1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.felipem.euler.problem1;
+
+/**
+ * @author felipe
+ *
+ */
+public class Problem1 {
+
+	/**
+	 * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+	 * Find the sum of all the multiples of 3 or 5 below 1000.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int suma = 0;
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				suma+=i;
+			}
+		}
+		System.out.println(suma);
+	}
+
+}
