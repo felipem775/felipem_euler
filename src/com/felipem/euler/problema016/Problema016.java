9ce0b545c5e95e18c5510e31d73615d68ca43c1b
Felipe Maza <felipe@felipem.com>
2012-10-02 12:07:46 +0200

    Problema016 resuelto

---
+++ src/com/felipem/euler/problema016/Problema016.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.felipem.euler.problema016;
+
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+
+/**
+ * @author felipem ( http://felipem.com )
+ *
+ */
+public class Problema016 {
+
+	/**
+	 * 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+	 * What is the sum of the digits of the number 21000?
+	 * 
+	 * 2^15 = 32768 y la suma de sus dígitos es 3 + 2 + 7 + 6 + 8 = 26.
+	 * ¿Cuál es la suma de los dígitos del número 2^1000?
+	 *  
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		BigInteger numero = BigInteger.valueOf(2);
+		numero = numero.pow(1000);
+		
+		String cad = numero.toString();
+		int contador = 0;
+		
+		for (int i = 0; i < cad.length(); i++) {
+			contador += Integer.parseInt(cad.substring(i, i+1));
+		}
+		
+		System.out.println(contador);
+	}
+
+}
